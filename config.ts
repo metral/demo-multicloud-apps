322a85a5093aa40e020325f47d28bab2abe80cbd
Mike Metral <1112768+metral@users.noreply.github.com>
2019-09-25 22:23:10 -0700

    init commit

--- config.ts
+++ config.ts
@@ -2,9 +2,9 @@ import * as pulumi from "@pulumi/pulumi";
 
 let pulumiConfig = new pulumi.Config();
 
-// Reference the deployed demo-multicloud-clusters Pulumi stack.
+// Reference the deployed multicloud Pulumi stack.
 // A stack reference is required in the format:
-// <organization_or_user>/<project>/<stack> e.g. "myuser/projectName/stackName"
+// <organization_or_user>/<projectName>/<stackName> e.g. myUsername/projectName/stackName
 const stackReferenceName = pulumiConfig.require("stackReferenceName");
 const multicloud = new pulumi.StackReference(stackReferenceName);
 
